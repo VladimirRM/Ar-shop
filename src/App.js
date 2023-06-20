d7153907d4109a7e14d59bd4127c2ee987c9850e
Vladimir RM <shvdrummer@gmail.com>
2023-06-20 13:50:31 +0300

    Add new method render use map

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import './index.scss'
 import Card from './Components/Card';
 import Headers from './Components/Headers';
+import Drawer from './Components/Drawer';
+
+const arr = [
+  {name:'Мужские Кроссовки Nike Blazer Mid Suede' ,price: 12999 },
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 13999}
+  
+]
 
 function App() {
+
+
   return (
     <div className="wrapper clear">
       <Headers/>
-      
+      <Drawer/>
       
   
       <div className="content p-40">
@@ -20,23 +29,6 @@ function App() {
            </div>
         </div>
         <div className='d-flex'>
-      
-        
-    
-    
-        <div className="card">
-          <img  width={133}height={112} src="/img/4.jpeg" alt="Sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className='d-flex justify-between aline-center'>
-            <div className='d-flex  flex-column '>
-              <span>Price</span>
-              <b>12 999grn</b>
-            </div>
-            <button className='button'>
-              <img   width={11}  height={11}  src="/img/plus.svg" alt="" />
-            </button>
-          </div>
-        </div>
       </div>
       <Card/>
       </div>
