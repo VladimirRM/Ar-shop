1751140da27024812195f88f1741dc0193642068
Vladimir RM <shvdrummer@gmail.com>
2023-06-21 11:04:50 +0300

    Correct product name

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Headers from './Components/Headers';
 import Drawer from './Components/Drawer';
 
 const arr = [
-  {name:'Мужские Кроссовки Nike Blazer Mid Suede' ,price: 12999 },
-  {name: 'Мужские Кроссовки Nike Air Max 270', price: 13999}
-  
+  {title:'Мужские Кроссовки Nike Blazer Mid Suede' ,price: 12999,imageUrl: '/img/1.jpeg' },
+  {title: 'Мужские Кроссовки Nike Air Max 270', price: 13999,imageUrl: '/img/2.jpeg'},
+  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 15900,imageUrl: '/img/3.jpeg'},
+  {title: 'Мужские Кроссовки Under Armour Curry 8', price: 10900,imageUrl: '/img/4.jpeg'},
+
 ]
 
 function App() {
@@ -29,8 +31,17 @@ function App() {
            </div>
         </div>
         <div className='d-flex'>
+          {arr.map((obj)=>(
+        <Card
+        title={obj.name}
+        price={obj.price}
+        imageUrl={obj.imageUrl}
+        />
+
+          ))}
+
       </div>
-      <Card/>
+
       </div>
     </div>
 
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props. imageUrl)
   return (
     <div>
       <div className="card">
@@ -8,12 +9,12 @@ const Card = () => {
 <img src="/img/heart-unliked.svg" alt="Unliked" />
 
 </div>
-<img  width={133}height={112} src="/img/1.jpeg" alt="Sneakers" />
-<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+<img  width={133}height={112} src={props. imageUrl} alt="Sneakers" />
+<h5>{props.title}</h5>
 <div className='d-flex justify-between aline-center'>
   <div className='d-flex  flex-column '>
     <span>Price</span>
-    <b>12 999grn</b>
+    <b>{props.price}</b>
   </div>
   <button className='button'>
     <img   width={11}  height={11}  src="/img/plus.svg" alt="" />
